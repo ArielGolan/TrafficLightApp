5645a794609fb115f9bbdca0ae89d1d6d20048f9
Ariel Golan <ariel.golan.ag@gmail.com>
2022-06-19 23:17:23 +0300

    HW 2.02 Traffic light app

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redColorView: UIView!
+    @IBOutlet var yellowColorView: UIView!
+    @IBOutlet var greenColorView: UIView!
+    
+    @IBOutlet var startBottun: UIButton!
+    
+    enum CurrentLightColor {
+        case red, yellow, green
+    }
+    
+    var currentLight = CurrentLightColor.red
+    let lightIsOn: CGFloat = 1
+    let lightIsOff: CGFloat = 0.2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redColorView.alpha = lightIsOff
+        yellowColorView.alpha = lightIsOff
+        greenColorView.alpha = lightIsOff
+        
+        startBottun.layer.cornerRadius = 10
+        
+        redColorView.layer.cornerRadius = redColorView.frame.size.width / 2
+        yellowColorView.layer.cornerRadius = yellowColorView.frame.size.width / 2
+        greenColorView.layer.cornerRadius = greenColorView.frame.size.width / 2
     }
 
-
+    @IBAction func startButtonTapped() {
+        
+        if startBottun.currentTitle == "START" {
+            startBottun.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+            
+        case .red:
+            greenColorView.alpha = lightIsOff
+            redColorView.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redColorView.alpha = lightIsOff
+            yellowColorView.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowColorView.alpha = lightIsOff
+            greenColorView.alpha = lightIsOn
+            currentLight = .red
+        }
+    }
+    
 }
 
